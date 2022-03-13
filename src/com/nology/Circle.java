8e182b5007a14aa19b8bce5295c3fe176ab52a50
Allan Shrestha <stevevai111@hotmail.com>
2022-03-13 17:56:59 +0000

    Circle class updated for correct calculation

--- src/com/nology/Circle.java
+++ src/com/nology/Circle.java
@@ -1,21 +1,28 @@
 package com.nology;
 
 public class Circle extends Shape {
-    public Circle(int numOfSides) {
+
+    int radius;
+
+    public Circle(int numOfSides, int radius) {
         super(numOfSides);
+        this.radius = radius;
     }
 
-    double numOfSides = 2 * Math.PI * getNumOfSides();
+    public int getRadius() {
+        return radius;
+    }
 
     @Override
     public void getArea() {
-        double Area = Math.PI * getNumOfSides() * getNumOfSides();
+        double Area = Math.PI * getRadius() * getRadius();
         System.out.println("The area of circle is " + Area);
     }
 
     @Override
     public void getPerimeter() {
-        System.out.println("The perimeter of circle is " + numOfSides);
+        double Perimeter = 2 * Math.PI * getRadius();
+        System.out.println("The perimeter of circle is " + Perimeter);
     }
 
 
--- src/com/nology/Main.java
+++ src/com/nology/Main.java
@@ -3,7 +3,7 @@ package com.nology;
 public class Main {
 
     public static void main(String[] args) {
-	 Circle circle = new Circle(0);
+	 Circle circle = new Circle(1, 3);
      Rectangle rectangle = new Rectangle(4, 5,10);
      Square square = new Square(4, 2, 2);
      Triangle triangle = new Triangle(3, 4, 5, 7, 11);
